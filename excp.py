5e440056df1384f61e68414006a7a8054c0d705b
Pranay <pranayvjadhao197@gmail.com>
2026-06-25 23:37:20 +0530

    Initial commit

---
+++ excp.py
@@ -0,0 +1,13 @@
+def test():
+    try:
+        raise AttributeError("Underlying exception")
+    except AttributeError as cause:
+        raise ValueError("Main Exception") from cause
+
+
+try:
+    test()
+
+except ValueError as ex:
+    print("Main Exception :", ex)
+    print("Underlying exception :", ex.__cause__)
